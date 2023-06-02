b2300122749ea922893198c1e061724c0c5e3f5b
taesamja <taesamja12345@gmail.com>
2023-06-02 16:13:37 +0900

    3장 첫번째 작업

---
+++ ch3_01.c
@@ -0,0 +1,34 @@
+// search inode information by filename
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() 
+{
+    struct stat statbuf;
+
+    stat("linux.txt", &statbuf);
+
+    printf("Inode = %d\n", (int)statbuf.st_ino);
+    printf("Mode = %o\n", (unsigned) statbuf.st_mode);
+    printf("Nlink = %o\n", (unsigned) statbuf.st_nlink);
+    printf("UID = %d\n", (int)statbuf.st_uid);
+    printf("GID = %d\n", (int)statbuf.st_gid);
+    printf("SIZE = %d\n", (int)statbuf.st_size);
+    printf("Blksize = %d\n", (int)statbuf.st_blksize);
+    printf("Blocks = %d\n", (int)statbuf.st_blocks);
+
+    printf("** timespec Style\n");
+    printf("Atime = %d\n", (int)statbuf.st_atim.tv_sec);
+    printf("Mtime = %d\n", (int)statbuf.st_mtim.tv_sec);
+    printf("Ctime = %d\n", (int)statbuf.st_ctim.tv_sec);
+    printf("** old Style\n");
+    printf("Atime = %d\n", (int)statbuf.st_atim.tv_sec);
+    printf("Mtime = %d\n", (int)statbuf.st_mtim.tv_sec);
+    printf("Ctime = %d\n", (int)statbuf.st_ctim.tv_sec);
+    
+
+    return 0;
+}
